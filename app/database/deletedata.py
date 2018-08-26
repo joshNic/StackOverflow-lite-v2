400f2bf8aafa00c0354ca7bac258758d8d103f3f
joshNic <256jomu@gmail.com>
2018-08-26 11:01:55 +0300

    add deletedata [Chore #160043324]

---
+++ app/database/deletedata.py
@@ -0,0 +1,34 @@
+import psycopg2
+from config import config
+
+
+def delete_user(user_id):
+    """ delete user by user id """
+    conn = None
+    rows_deleted = 0
+    try:
+        # read database configuration
+        params = config()
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the UPDATE  statement
+        cur.execute("DELETE FROM users WHERE user_id = %s", (user_id,))
+        # get the number of updated rows
+        rows_deleted = cur.rowcount
+        # Commit the changes to the database
+        conn.commit()
+        # Close communication with the PostgreSQL database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return rows_deleted
+
+
+if __name__ == '__main__':
+    delete_user(2)
