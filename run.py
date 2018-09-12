09ad75bee9879a3919e4b70ab9bf7493ab74a43e
joshNic <256jomu@gmail.com>
2018-09-12 08:17:35 +0300

    refactor

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from app.views import app
-from create_tables import create_tables
+# from create_tables import create_tables
 
 if __name__ == '__main__':
-    create_tables()
+    # create_tables()
     app.run()
     
 
